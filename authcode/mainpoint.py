4b17ed4929186e11735ac585afc9e8ad2bd22f5e
lokranjanp <lokranjan03@gmail.com>
2024-11-25 15:51:39 +0530

    prep mainpoint

--- authcode/mainpoint.py
+++ authcode/mainpoint.py
@@ -3,15 +3,21 @@ from otpmail import *
 from register import *
 from sessions import *
 
-def validate_input(validator, type):
-    return True
+# print("Registration : \n")
+# username = str(input("Enter your Username : "))
+# useremail = str(input("Enter your Email : "))
+# userpassword = str(input("Enter your Password : "))
+#
+# if (validate_input(validator=username, type="stringmix1") and validate_input(validator=useremail, type="stringmail")
+#         and validate_input(validator=userpassword, type="stringmix2")):
+#     if register_user(username, useremail, userpassword):
+#         print("Registration Successful")
 
-print("Registration : \n")
-username = str(input("Enter your Username : "))
-useremail = str(input("Enter your Email : "))
-userpassword = str(input("Enter your Password : "))
+path = "../.env"
+r = redis.StrictRedis(host=dotenv.get_key(path, "REDIS_HOST"),
+                      port=dotenv.get_key(path, "REDIS_PORT"),
+                      db=dotenv.get_key(path, "REDIS_DB"))
 
-if (validate_input(validator=username, type="stringmix1") and validate_input(validator=useremail, type="stringmail")
-        and validate_input(validator=userpassword, type="stringmix2")):
-    if register_user(username, useremail, userpassword):
-        print("Registration Successful")
+genotp = generate_otp("Loki")
+print(genotp)
+print(cache_otp(r, "pookie", genotp))
